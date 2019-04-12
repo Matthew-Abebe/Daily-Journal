7222ba835f422773f645a30d99710f0c37590e89
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-04-12 13:25:21 -0500

    dailyJournal

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+const objectsJournalEntry = {
+    date: "04/03/2019",
+    concepts: "html",
+    entry: "i learned html today",
+    mood: "great"
+};
+
+const journalEntries = [
+    {
+        date: "04/03/2019",
+        concepts: "html",
+        entry: "i learned html today",
+        mood: "great"
+    },
+    {
+        date: "04/08/2019", 
+        concepts: "css",
+        entry: "i learned css today",
+        mood: "good"
+    },
+    {
+        date: "04/09/2019",
+        concepts: "git",
+        entry: "i learned git today",
+        mood: "good"
+    }
+];
+journalEntries.push();
+console.log(journalEntries);
